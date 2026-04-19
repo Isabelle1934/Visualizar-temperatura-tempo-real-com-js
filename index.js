78bea73ce693d8ef3870cc5c9d88ac12079f8c91
Isabelle Silva <bellinha292@gmail.com>
2026-04-19 12:48:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const apikey = "8025f76f91042ccf362f8221da2a0563";
 const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+  });
